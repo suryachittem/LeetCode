81b3f159a544a4345aa4977283a62136b1aa28c6
SURYA <suryachittem80@gmail.com>
2024-06-12 23:09:46 +0530

    fourth commit

---
+++ mountainArray.java
@@ -0,0 +1,26 @@
+public class mountainArray {
+    public static void main(String[] args){
+        int[] arr={1,2,4,5,3,2};
+        System.out.println(peakmountainarrauyu(arr));
+    }
+    static int peakmountainarrauyu(int[] arr)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid+1])
+            {
+                end=mid;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+
+}
+
